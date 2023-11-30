4dff0bc2493956f8998fd88771f41af4999c8586
IshaPatade <ishaapatade14@gmail.com>
2023-11-30 22:37:09 +0530

    first 20 mins

---
+++ contracts/Tracking.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Tracking {
+    enum ShipmentStatus {
+        PENDING,
+        IN_TRANSIT,
+        DELIVERED
+    }
+    struct Shipment {
+        address sender;
+        address receiver;
+        uint256 pickupTime;
+        uint256 deliveryTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool isPaid;
+    }
+
+    mapping(address => Shipment[]) public shipments;
+    uint256 public shipmentCount;
+
+    struct TypeShipment {
+        address sender;
+        address receiver;
+        uint256 pickupTime;
+        uint256 deliveryTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool isPaid;
+    }
+
+    TypeShipment[] typeShipments;
+
+    event ShipmentCreated(
+        address indexed sender,
+        address indexed receiver,
+        uint256 pickupTime,
+        uint256 distance,
+        uint256 price
+    );
+
+     event ShipmentInTransit(
+        address indexed sender,
+        address indexed receiver,
+        uint256 pickupTime,
+        
+    );
+     event ShipmentDelivered(
+        address indexed sender,
+        address indexed receiver,
+        uint256 deliveryTime,
+        
+    );
+     event ShipmentPaid(
+        address indexed sender,
+        address indexed receiver,
+        uint256 amount  
+    );
+
+    constructor(){
+        shipmentCount =0;    
+    }
+    function createShipment(address _receiver, uint256 _pickupTime, uint256 _distance, uint256 _price)public payable{
+        require(msg.value == _price, "Payment amount must match the price");
+
+        Shipment memory shipment = Shipment(msg.sender, _receiver, _pickupTime, 0, _distance, _price, ShipemntStatus.PENDING, false);
+         
+         shipments[msg.sender].push(shipment);
+         shipmentCount++;
+
+         typeShipments.push(
+            TypeShipemnt(
+                msg.sender,
+                _receiver,
+                _pickupTime,
+                0,
+                _distance,
+                _price,
+                ShipmentStatus.PENDING,
+                false
+            )
+         );
+
+         emit ShipmentCreated(msg.sender, _receiver, _pickupTime, _distance, _price);
+
+    }
+    
+    function startShipment(address _sender, address _receiver, uint256 _index) public{
+        Shipment storage shipment= shipments[_sender][_index];
+        TypeShipment storage typeShipment = typeShipments[_index];
+
+        require(shipment.receiver == _receiver, "Invalid receiver.");
+        require(shipment.status == ShipmentStatus.PENDING, "Shipment already in transit.");
+
+        shipment.status =ShipmentStatus.IN_TRANSIT;
+        typeShipemnt.status = ShipmentStatus.IN_TRANSIT;
+
+        emit ShipmentInTransit(_sender, _receiver, shipment.pickupTime);
+
+    }
+
+    function completeShipment(address _sender,address  _receiver, uint256 _index) public {
+        Shipment storage shipment = shipments[_sender][_index];
+        Typeshipment storage typeShipment = typeShipments[_index];
+
+        require(shipment.receiver == _receiver, 'Invalid receiver');
+        require(shipment.status == ShipemntStatus.IN_TRANSIT, 'Shipment not in transit.');
+        require(!shipment.isPaid, "Shipment already paid.");
+
+        shipment.status = ShipmentStatus.DELIVERED;
+        typeShipment.status = ShipmentStatus.DELIVERED;
+        typeShipment.deliveryTime = block.timestamp;
+        shipemnt.deliveryTime = block.timestamp;
+
+        uint256 amount = shipment.price;
+
+        payable(shipment.sender).transfer(amount);
+
+        shipment.isPaid = true;
+        typeShipment.isPaid = true;
+
+        emit ShipmentDelivered(_sender, _receiver, shipment.deliveryTime);
+        emit ShipmentPaid(_sender, _receiver, amount );
+ 
+    }
+
+    
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+
+const hre = require("hardhat");
+
+async function main() {
+ 
+
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+   `Tracking deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
