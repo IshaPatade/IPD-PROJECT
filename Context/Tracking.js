0c59e3c2d84094d1a302a412185a447bc8badc4f
AditKanaji <aditbkanaji@gmail.com>
2023-12-02 12:38:06 +0530

    Till

--- Context/Tracking.js
+++ Context/Tracking.js
@@ -65,10 +65,164 @@ export const TrackingProvider = ({ children }) => {
       console.log("error went, getting shipping");
     }
   };
-
   const getShipmentCount = async () => {
     try {
-      // START HERE
-    } catch (error) {}
+      if (!window.ethereum) return "Install MetaMask";
+
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      const provider = new ethers.providers.JsonRpcProvider();
+      const contract = fetchContract(provider);
+      const shipmentsCount= await contract.getShipmentCount(accounts[0]);
+      return shipmentsCount.toNumber();
+    } catch (error) {
+      console.log("error want, getting shipment");
+    }
   };
-};
+  const completeShipment = async(completeShip) => {
+    console.log(completeShip);
+
+    const { receiver, index } = completeShip;
+    try{
+      if(!window.ethereum) return "Install MetaMask";
+
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = fetchContract(signer);
+
+      const transaction = await contract.completeShipment(
+        accounts[0],
+        receiver,
+        index,
+        {
+          gasLimit: 300000,
+        }
+      );
+      transaction.wait();
+      console.log(transaction);
+    } catch(error){
+      console.log('wrong completeShipment', error);
+    }
+  };
+
+  const getShipment = async (index) => {
+    console.log(index * 1);
+    try{
+      if(!window.ethereum) return "Install MetaMask";
+
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+
+      const provider = new ethers.providers.JsonRpcBatchProvider();
+      const contract = fetchContract(provider);
+      const shipment = await contract.getShipment(accounts[0], index * 1);
+
+      const SingleShipment = {
+        sender:shipment[0],
+        receiver: shipment[1],
+        pickupTime:shipment[2].toNumber(),
+        deliveryTime:shipment[3].toNumber(),
+        distance:shipment[4].toNumber(),
+        price: ethers.utils.formatEther(shipment[5].toString()),
+        status:shipment[6],
+        isPaid: shipment[7],
+      };
+
+      return SingleShipment;
+    }catch(error) {
+      console.log("Sorry no chipment");
+    }
+  };
+
+  const startShipment = async (getProduct) => {
+    const{ receiver , index } = getProduct;
+
+    try{
+      if(!window.ethereum) return "Install Metamask";
+
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const shipment = await contract.startShipment(
+        accounts[0],
+        receiver,
+        index*1
+      );
+      shipment.wait();
+      console.log(shipment);
+    }catch(error) {
+      console.log("Sorry no shipment",error);
+    }
+  };
+  //--CHECK WALLET CONNECTED
+  const checkIfWalletConnected = async () => {
+    try{
+      if(!window.ethereum) return "Install MetaMask";
+
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+
+      if(accounts.length){
+        setCurrentUser(accounts[0]);
+      }else{
+        return "No account";
+      }
+    }catch(error){
+      return "not connected";
+    }
+  };
+
+  //---CONNECT WALLET FUNCTION
+
+  const connectWallet = async () => {
+    try{
+      if(!window.ethereum) return "Install MetaMask";
+
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+
+      });
+
+      setCurrentUser(accounts[0]);
+    }catch(error){
+      return "Something went wrong"
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletConnected();
+  }, []);
+
+  return(
+    <TrackingContext.Provider 
+    value={{
+      connectWallet,
+      createShipment,
+      getAllShipment,
+      completeShipment,
+      getShipment,
+      startShipment,
+      getShipmentCount,
+      DappName,
+      currentUser,
+    }}
+   >
+   {children}
+   </TrackingContext.Provider>
+  );
+    
+
+}
