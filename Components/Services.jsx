60f07853bea0dd507e468144ebfeb5ed4a94796d
vaishkadukar <vgkadukar21@gmail.com>
2023-12-03 23:30:19 +0530

    Images upload

--- Components/Services.jsx
+++ Components/Services.jsx
@@ -1,78 +1,76 @@
-import React from 'react'
+import React from "react";
 
-const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+// const Services = () => {
+//   return (
+//     <div>Services</div>
+//   )
+// }
 
-export default Services
+// export default Services
 
 // import images from "..?Images/index.js";
-// // import Images from " next'images";
-// import { MODAL_CARD_CLASSNAME } from "web3modal";
+// import Images from " next'images";
+import { MODAL_CARD_CLASSNAME } from "web3modal";
 
-// export default ({
-//   setOpenProfile,
-//   setCompleteModal,
-//   setGetModel,
-//   setStartModal,
-// }) => {
-//   const team = [
-//     {
-//       avatar: images.compShipment,
-//     },
-//     {
-//       avatar: images.getShipment,
-//     },
-//     {
-//       avatar: images.startShipment,
-//     },
-//     {
-//       avatar: images.userProfile,
-//     },
-//     {
-//       avatar: images.shipCount,
-//     },
-//     {
-//       avatar: images.send,
-//     },
-//   ];
-//   const openModelBox = (text)=> {
-//     if(text === 1){
-//       setCompleteModal(true);
-//     }else if (text === 2){
-//       setGetModal(true);
-//     }else if (text === 3){
-//       setStartModal(true);
-//     } else if (text === 4){
-//       setOpenProfile(true);
-//     }   
-//   };
-//   return (
-//     <section className="py=0 pb-14">
-//       <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-//         <div className="mt-12">
-//           <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-//             {team.map((item,i) => (
-//               <li key={i}>
-//                 <div
-//                   onClick={ () => openModelBox(i+1)}
-//                   className='w-full h-60 sm:h-52 md:h-56'
-//                 >
-//                   <Image 
-//                     src={item.avatar}
-//                     className="w-full h-full object-cover object object-center shadow-md rounded-xl"
-//                     alt=""
-//                     />
-//                   </div>
-//                 </li>
-              
-//             ))}
-//           </ul>
-
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
+export default ({
+  setOpenProfile,
+  setCompleteModal,
+  setGetModel,
+  setStartModal,
+}) => {
+  const team = [
+    {
+      avatar: images.compShipment,
+    },
+    {
+      avatar: images.getShipment,
+    },
+    {
+      avatar: images.startShipment,
+    },
+    {
+      avatar: images.userProfile,
+    },
+    {
+      avatar: images.shipCount,
+    },
+    {
+      avatar: images.send,
+    },
+  ];
+  const openModelBox = (text) => {
+    if (text === 1) {
+      setCompleteModal(true);
+    } else if (text === 2) {
+      setGetModal(true);
+    } else if (text === 3) {
+      setStartModal(true);
+    } else if (text === 4) {
+      setOpenProfile(true);
+    }
+  };
+  return (
+    <section className="py=0 pb-14">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="mt-12">
+          <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
+            {team.map((item, i) => (
+              <li key={i}>
+                <div
+                  onClick={() => openModelBox(i + 1)}
+                  className="w-full h-60 sm:h-52 md:h-56"
+                >
+                  <Image
+                    src={item.avatar}
+                    className="w-full h-full object-cover object object-center shadow-md rounded-xl"
+                    alt=""
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
